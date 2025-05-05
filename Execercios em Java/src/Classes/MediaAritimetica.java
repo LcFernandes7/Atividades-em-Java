47952d470d841a18acac03e1407addb7708f16de
Lucas de Oliveira Fernandes <lucas.fernandes@BTNWAWS.CORP>
2025-05-05 16:22:01 -0300

    05/05/2025 Desenvolvimento da primeira atividade de media aritimetica

---
+++ Execercios em Java/src/Classes/MediaAritimetica.java
@@ -0,0 +1,42 @@
+package Classes;
+
+import javax.swing.*;
+
+public class MediaAritimetica {
+
+
+    public  void formula() {
+        String nome;
+        double n1 = 0, n2 = 0;
+
+        while (true) {
+            nome = JOptionPane.showInputDialog("Digite o seu nome:");
+            if (nome.matches("[a-zA-Z\\s]+")) break;
+            JOptionPane.showMessageDialog(null ,"Nome inválido. Digite apenas letras.");
+        }
+
+        while (true) {
+            String n1String = JOptionPane.showInputDialog("Digite o valor da primeira prova:");
+            if (n1String != null && n1String.matches("\\d+,\\d+")) {
+                 n1 = Double.parseDouble(n1String.replace("," , "."));
+                break;
+            } else {
+                JOptionPane.showMessageDialog(null, "Digite um número valido!");
+            }
+        }
+
+        while (true) {
+            String n2String = JOptionPane.showInputDialog("Digite o valor da segunda prova:");
+            if (n2String != null && n2String.matches("\\d+,\\d+")) {
+                 n2 = Double.parseDouble(n2String.replace(",", "."));
+                 break;
+            }else {
+                JOptionPane.showMessageDialog(null,"Digite um número valido!");
+            }
+        }
+
+        double media = (n1 + n2) / 2.0;
+        JOptionPane.showMessageDialog(null, "Sua média foi: " + media);
+
+    }
+}
---
+++ Execercios em Java/src/Main.java
@@ -0,0 +1,11 @@
+import Classes.MediaAritimetica;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MediaAritimetica media = new MediaAritimetica();
+
+        media.formula();
+
+    }
+}
